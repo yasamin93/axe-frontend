0766a4c5be504bf882e4d427e6c964b56de83dfa
yasie72 <27311024+yasie72@users.noreply.github.com>
2023-05-28 23:49:25 +0200

    [ax-3] connect to manage page.

--- axe-frontend/src/components/AppHeader.vue
+++ axe-frontend/src/components/AppHeader.vue
@@ -3,7 +3,9 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
+        >AX Visions</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
@@ -15,7 +17,10 @@
             >
           </li>
           <li>
-            <a class="px-2 text-white" href="#">Manage</a>
+            <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+          </li>
+          <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
           </li>
         </ul>
       </div>
